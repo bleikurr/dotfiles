f7dd8fb28ee1d8e6f237e20cee73ae2e428ed9a5
Frosti Grétarsson <gretarssonur@gmail.com>
2026-04-15 16:15:09 +0000

    Fix nvim background

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -6,11 +6,13 @@ vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.wildmode = { "longest", "list" }
 vim.opt.cc = { 80 }
-vim.opt.bg = "dark"
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.number = true
 
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE", ctermbg = "NONE" })
 
 vim.cmd("filetype plugin on")
 vim.cmd("filetype plugin indent on")
